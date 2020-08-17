4559d66fcef104d90bb2bf5b764b1db776dabe5b
lidada <ll666634@163.com>
2020-08-17 19:45:10 +0800

    创建分支

--- src/main/java/com/cblue/demo.java
+++ src/main/java/com/cblue/demo.java
@@ -11,5 +11,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello,springcloud");
         System.out.println("hello,sprngcloud---2");
+        System.out.println("创建分支");
     }
 }
