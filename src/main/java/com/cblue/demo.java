b7f941698bbda781f554171fed24d5262c1fb9a6
lidada <ll666634@163.com>
2020-08-17 19:30:16 +0800

    添加git本地库

---
+++ src/main/java/com/cblue/demo.java
@@ -0,0 +1,14 @@
+package com.cblue;
+
+/**
+ * @author lidada
+ * @Description TODO
+ * @create demo
+ * @date 2020/8/17 - 星期一 - 11:51
+ */
+//面试题练习
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello,springcloud");
+    }
+}
---
+++ src/main/java/com/cblue/test.java
@@ -0,0 +1,15 @@
+package com.cblue;
+
+/**
+ * @author lidada
+ * @Description TODO
+ * @create test
+ * @date 2020/7/29 - 星期三 - 13:50
+ */
+public class test {
+    public static void main(String[] args) {
+        String str = "hello";
+        String str2 = "he" + new String("llo");
+        System.out.println(str == str2);
+    }
+}
